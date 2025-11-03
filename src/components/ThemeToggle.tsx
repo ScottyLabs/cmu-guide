a791b632c8ecf276e13e90871be4aef202ea91ab
Anish Pallati <i@anish.land>
2025-11-02 19:28:29 -0500

    feat: add github link

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,4 +1,5 @@
-import { Sun, Moon01 } from '@untitledui/icons';
+import { Button } from "@/components/Button";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
 
 export default function ThemeToggle() {
 	const onClick = () => {
@@ -7,13 +8,10 @@ export default function ThemeToggle() {
 	};
 
 	return (
-		<button
-			onClick={onClick}
-			className="relative p-2 rounded-lg bg-white border border-gray-200 hover:bg-gray-100 dark:bg-zinc-700 dark:border-zinc-500 dark:hover:bg-zinc-600 transition-colors"
-		>
-			<Sun className="size-5 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0" />
-			<Moon01 className="size-4.5 stroke-white absolute inset-0 m-auto transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100" />
+		<Button onClick={onClick} className="relative">
+			<SunIcon className="size-5 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0" />
+			<MoonIcon className="size-5 stroke-white absolute inset-0 m-auto transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100" />
 			<span className="sr-only">Toggle theme</span>
-		</button>
+		</Button>
 	);
 }
