25c75c747d30a88002aaebca270eabbd00a61633
Zeyu Yao <cytronicoder+code@gmail.com>
2026-07-13 21:56:33 +0800

    feat: update SearchCommand and ThemeToggle styles

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -10,10 +10,10 @@
 
 <Button onclick={onClick} class="relative">
 	<Sun
-		class="size-6 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0"
+		class="size-6 text-ui-icon transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0"
 	/>
 	<Moon
-		class="size-6 dark:invert absolute inset-0 m-auto transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100"
+		class="size-6 text-ui-icon absolute inset-0 m-auto transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100"
 	/>
 	<span class="sr-only">Toggle theme</span>
 </Button>
